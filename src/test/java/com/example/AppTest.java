23fa9c41b6948a50ed0d03b5bb24918ba6d3d09a
atul <atullimbale11@gmail.com>
2026-09-22 12:11:37 +0000

    Initial Jenkins CI project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testMessage() {
+        assertEquals("Hello from Jenkins CI!", App.message());
+    }
+}
