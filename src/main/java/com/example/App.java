f2dab649418d66f79b8fb85e7c9a0f6b7dadc2da
atul <atullimbale11@gmail.com>
2026-09-22 12:44:32 +0000

    Update application message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,8 +2,8 @@ package com.example;
 
 public class App {
 
-    public static String message() {
-        return "Hello from Jenkins CI!";
+    public static String message() { return "Hello from Jenkins CI -Build 2!";
+
     }
 
     public static void main(String[] args) {
